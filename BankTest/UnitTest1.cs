b76aafebbea985f3a886862e62b686107d1f57dd
Davidsiltroy <davidsiltroy2008@hotmail.com>
2022-03-31 13:43:43 +0200

    Testing some methods

--- BankTest/UnitTest1.cs
+++ BankTest/UnitTest1.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using Bank;
+
 namespace BankTest
 {
     [TestClass]
@@ -8,6 +10,27 @@ namespace BankTest
         [TestMethod]
         public void TestMethod1()
         {
+            //ARRANGE: Initialize data
+            Account account = new Account();
+
+            //ACT: Method calls
+            double balance = account.Blance;
+
+            //ASSERT: Test the result is as expected
+            Assert.AreEqual(0, balance);
+
+        }
+        [TestMethod]
+        public void Credit_999OnBalance0_Returns999()
+        {
+            //ARRANGE: Initialize data
+            Account account = new Account();
+
+            //ACT: Method calls
+            account.Credit(999);
+
+            //ASSERT: Test the result is as expected
+            Assert.AreEqual(999, account.Blance);
         }
     }
 }
